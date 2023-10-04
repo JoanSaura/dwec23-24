7c838707e2b1883887c2f9aa987a22aa023029a5
JoanSaura <jsaura20489@iesjoanramis.org>
2023-10-04 20:59:18 +0200

    Ej 2.1

--- u3/ejercicio.js
+++ u3/ejercicio.js
@@ -21,11 +21,14 @@ console.log("Nota del proyecto : "+notaProjecte );
 console.log("Nota final : " +NotaFinal);
 //Comprovamos si el estudiante ha aprovado
  if (NotaFinal >=7 ) {
+    //Felicita si has sacado un 7 o superior
     console.log("Felicidades " +nomEstudiant +" Has aprobado el modulo");
 }
 else if(NotaFinal >=5) {
+    //Si has sacado entre un 5 y un 7 te dira que has aprobado pero que has de esforcarse mas
     console.log("Has aprobado el modulo " +nomEstudiant + ", Pero has de esforcarte mas para aprobar el modulo");
 } 
 else {
+    //No has aprobado
     console.log("No has aprobado el modulo " +nomEstudiant );
 }
