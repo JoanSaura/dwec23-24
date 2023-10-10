35931e9c7ef827b4553940e2bdc40dd67b4ae771
JoanSaura <jsaura20489@iesjoanramis.org>
2023-10-10 17:19:49 +0200

    2

---
+++ u4/Tablas/script.js
@@ -0,0 +1,25 @@
+// Declaramos las variables
+let tablas = new Array(9);
+
+// Inicializamos la matriz
+for (let i = 0; i < tablas.length; i++) {
+    tablas[i] = new Array(10);
+}
+
+// Llenamos la matriz con las tablas de multiplicar
+for (let fila = 0; fila < tablas.length; fila++) { 
+    for (let col = 0; col < tablas[fila].length; col++) {
+        tablas[fila][col] = (col + 1) * (fila + 1);
+    }
+}
+
+// Imprimimos las tablas línea por línea
+for (let fila = 0; fila < tablas.length; fila++) {
+    let linea = "";
+    let impares ;
+    for (let col = 0; col < tablas[fila].length; col++) {
+        if(tablas[fila][col] %2 ===)
+        linea += tablas[fila][col] + "\t"; // Usamos "\t" para separar los números con tabulaciones
+    }
+    console.log(linea);
+}
--- u4/script.js
+++ u4/script.js
@@ -1,28 +1,101 @@
+/*
+    ARRAYS
 
-//Array
-//Definir Arrays
-let vacio = []; //No tiene elementos
-let primos = [2,3,5,7,11]; //5 Elemtos numericos 
-let miscelania = [1.1, true, "openhaimer"]; //Con varios tipos de datos
+
+
+*/
+
+// Defniir arrays
+
+let vacio = []; //no tiene elementos
+let primos = [2, 3, 5, 7, 11]; //con 5 elementos numericos
+let miscelanea = [1.1, 4, true, null, "String"]; // varios tipos
+
+//los valores no tienen porque ser constantes
 let n = 1024;
-let tabla = [n,n*2,n*3  ];
-let conta = [1,3];
-console.log(conta.length);
+let tabla1 = [n, n + 1, n + 2, n + 3];
+
+let contador = [, , , , , ,];
+console.log(contador.length);
 
 //Operador SPREAD
-let spr = [1, 2 , 3];
-let spr2 = [0, 4 ];
 
-let origin = [1, 2 , 3];
-origin[0] = 9; //No cambiara el de la copia
-let copia  = [...origin,...spr2];
+let a = [1, 2, 3];
+let b = [0, ...a, 4];
 
-let digitos = ["...0123456789ABCDEF"];
+console.log(b);
+
+let original = [1, 2, 3];
+let copia = [...original, ...original];
+original[0] = 9;
+console.log(copia);
+console.log(original[0]);
+
+let digitos = [..."0123456789ABCDEF"];
+console.log(digitos);
 
 //Constructor de Array
-let a = new Array(1,2,3,4, 54, -1, "prueba", true);
-let b = Array.of(10); 
-a[119] = 10;
+let aa = new Array(1, 2, 3, 4, 54, -1, "prueba", true, "🤑🤑🤑🤑🤑🤑");
+let bb = Array.of(10, 3);
+console.log(aa[aa.length - 1]);
+console.log(aa.length);
+console.log(bb.length);
+aa[119] = 10;
+console.log(aa.length);
 
-//PUSH  
-a.push(5);
+//push añade elemento al final
+
+aa.push("hola");
+console.log(aa[aa.length - 1]);
+
+let aaa = [1, 2, 3, 4];
+console.log(aaa);
+delete aaa[2];
+console.log(aaa);
+let z = 3;
+if (a[z]) {
+  console.log(a[z]);
+} else {
+  console.log("Slot Vacio");
+}
+
+//slot vacio = false
+
+/*
+
+Iterando arrays
+
+*/
+
+let letras = [..."Hola amiguitos"];
+let aux = "";
+
+for (let letra of letras) {
+  aux += letra;
+}
+console.log(aux);
+aux = "";
+
+for (let [index, letra] of letras.entries()) {
+  if (index % 2 === 0) aux += letra;
+}
+console.log(aux);
+
+let mayusculas = "";
+let i = 0;
+letras.forEach((letra) => {
+  if (i % 2 === 0) mayusculas += letra.toUpperCase();
+  i++;
+});
+console.log(mayusculas);
+
+let vocales = "";
+let letra = "";
+for (let i = 0; i < letras.length; i++) {
+  letra = letras[i];
+
+  if (/[aeiou]/.test(letra)) {
+    vocales += letra;
+  }
+}
+console.log(vocales);
