b8eef8461112fc05895cd6eab09f68f10c7ed5bb
TommyDrug <jsaura20489@iesjoanramis.org>
2024-01-08 21:07:00 +0100

    React

---
+++ React/blog-dwec/src/Inicio.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+const Inicio = () => {
+    return ( 
+        <div className="home" >
+            <Navbar/>
+            <h2>Pagina de Inicio</h2>
+            
+        </div>
+     );
+}
+ 
+export default Inicio;
